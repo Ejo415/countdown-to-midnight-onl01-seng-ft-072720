b5a1d52560f0b807df0ef020d20ec356173f64f0
Eugene O'Neil <ejo7220@gmail.com>
2020-07-28 18:22:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
-seconds = 5
-def countdown(seconds)
+number = 5
+def countdown(number)
   while number > 0 
-  puts "#{seconds} SECOND(S)!"
+  puts "#{number} SECOND(S)!"
   seconds -= 1
   sleep(1)
   "HAPPY NEW YEAR!"
